30380afdd85cd2dcfcf76f6b41667002d0b09288
Michael Breedlove <me@michaelbreedlove.net>
2017-12-12 20:45:03 -0700

    redo zsh

--- install.sh
+++ install.sh
@@ -5,44 +5,59 @@ command -v realpath >/dev/null 2>&1 || realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-echo "Installing zprezto..."
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
-
-shopt -s extglob
-for rcfile in "${HOME}"/.zprezto/runcoms/!(README.md); do
-    ln -s "$rcfile" "${HOME}/.${rcfile##*/}"
-done
-
-rm -f ~/.zshrc
-ln -s "$(realpath ./zsh/zshrc)" "${HOME}/.zshrc"
-
-echo "Installing Vim configuration and plugins..."
-VIM_DIRS=( backup tmp )
-ln -s $(realpath ./vim) "${HOME}/.vim"
-for vimdir in "${VIM_DIRS[@]}"; do
-  mkdir "${HOME}/.vim/$vimdir"
-done
-vim +PlugInstall +qall
-
-echo "Installing tmux configuration..."
-for config in $(realpath ./tmux)/*; do
+##
+## Zsh
+##
+echo "Installing Zsh configuration..."
+mkdir -p $HOME/.zsh
+for config in $(realpath ./zsh)/*; do
   ln -s "$config" "${HOME}/.${config##*/}"
 done
 
-echo "Installing tmux package manager..."
-git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
-tmux new-session \; run-shell "${HOME}/.tmux/plugins/tpm/bindings/install_plugins" \; kill-server
+echo "Installing Antigen..."
+git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
 
-echo "Installing git configuration..."
-for config in $(realpath ./git)/*; do
-  ln -s "$config" "${HOME}/.${config##*/}"
-done
+###
+### Vim
+###
+#echo "Installing Vim configuration and plugins..."
+#ln -s $(realpath ./vim) "${HOME}/.vim"
 
-echo "Installing Powerline fonts..."
-git clone https://github.com/powerline/fonts.git
-cd fonts/
-./install.sh
-cd ..
-rm -rf fonts
+## Make required directories
+#VIM_DIRS=( backup tmp )
+#for vimdir in "${VIM_DIRS[@]}"; do
+  #mkdir "${HOME}/.vim/$vimdir"
+#done
+#vim +PlugInstall +qall
 
-echo "Done!"
+###
+### Tmux
+###
+#echo "Installing tmux configuration..."
+#for config in $(realpath ./tmux)/*; do
+  #ln -s "$config" "${HOME}/.${config##*/}"
+#done
+
+#echo "Installing tmux package manager..."
+#git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+#tmux new-session \; run-shell "${HOME}/.tmux/plugins/tpm/bindings/install_plugins" \; kill-server
+
+###
+### Git
+###
+#echo "Installing git configuration..."
+#for config in $(realpath ./git)/*; do
+  #ln -s "$config" "${HOME}/.${config##*/}"
+#done
+
+###
+### Powerline Fonts
+###
+#echo "Installing Powerline fonts..."
+#git clone https://github.com/powerline/fonts.git --depth=1
+#cd fonts/
+#./install.sh
+#cd ..
+#rm -rf fonts
+
+#echo "Done!"
